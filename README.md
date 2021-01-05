14a30b3a4b30f39f8b7cd1f2211b73f401268aba
kadoshita <sublimer.me@gmail.com>
2021-01-05 18:12:46 +0900

    add web page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# html5-template
+# endemic-area-dashboard
+
+豊橋技術科学大学の基準での流行地域を表示するダッシュボード
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,53 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import '../css/style.css';
+import * as d3 from 'd3';
 
 (async () => {
-    console.log('sample');
+    const mapElm = document.getElementById('map');
+    const updateTimestamp = document.getElementById('update-timestamp');
+    const endemicAreaRes = await fetch('/data/endemic-area.json');
+    const endemicArea = await endemicAreaRes.json();
+
+    updateTimestamp.innerText = `最終更新日時: ${endemicArea.timestamp}`;
+
+    mapElm.style.height = `${mapElm.offsetWidth}px`;
+    const width = mapElm.offsetWidth;
+    const height = mapElm.offsetWidth;
+    const centerPos = [137.0, 38.2];
+    const scale = mapElm.offsetHeight * 2.5;
+
+    const projection = d3
+        .geoMercator()
+        .center(centerPos)
+        .translate([width / 2, height / 2])
+        .scale(scale);
+
+    const path = d3.geoPath().projection(projection);
+
+    const svg = d3
+        .select('#map')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .attr('width', '100%')
+        .attr('height', '100%');
+
+    const geoJson = await d3.json('/data/japan.geo.json');
+
+    svg
+        .selectAll('path')
+        .data(geoJson.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('stroke', '#666')
+        .attr('stroke-width', 0.25)
+        .attr('fill', '#AD232F')
+        .attr('fill-opacity', item => {
+            if (endemicArea.area.includes(item.properties.name_ja)) {
+                return 1.0;
+            } else {
+                return 0.0;
+            }
+        });
 })();
