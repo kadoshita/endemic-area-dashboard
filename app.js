eeb3a52d3e776ebf34f79867de1a4aac1f263df4
kadoshita <sublimer.me@gmail.com>
2021-01-08 21:50:20 +0900

    update check except prefecture

--- app.js
+++ app.js
@@ -1,6 +1,56 @@
 const fetch = require('node-fetch');
 const fs = require('fs/promises');
 
+const PREFECTURE_LIST = [
+    '北海道',
+    '青森県',
+    '岩手県',
+    '宮城県',
+    '秋田県',
+    '山形県',
+    '福島県',
+    '茨城県',
+    '栃木県',
+    '群馬県',
+    '埼玉県',
+    '千葉県',
+    '東京都',
+    '神奈川県',
+    '新潟県',
+    '富山県',
+    '石川県',
+    '福井県',
+    '山梨県',
+    '長野県',
+    '岐阜県',
+    '静岡県',
+    '愛知県',
+    '三重県',
+    '滋賀県',
+    '京都府',
+    '大阪府',
+    '兵庫県',
+    '奈良県',
+    '和歌山県',
+    '鳥取県',
+    '島根県',
+    '岡山県',
+    '広島県',
+    '山口県',
+    '徳島県',
+    '香川県',
+    '愛媛県',
+    '高知県',
+    '福岡県',
+    '佐賀県',
+    '長崎県',
+    '熊本県',
+    '大分県',
+    '宮崎県',
+    '鹿児島県',
+    '沖縄県'
+];
+
 const main = async () => {
     const res = await fetch('https://www.tut.ac.jp/info/corona/katudo.html');
     const data = await res.text();
@@ -9,7 +59,19 @@ const main = async () => {
         throw 'response data error';
     }
 
-    const areaList = endemicArea[1].split('、');
+    const endemicAreaText = endemicArea[1];
+
+    const isExcept = /.*を除く都道府県$/.test(endemicAreaText);
+
+    let areaList = [];
+    if (isExcept) {
+        const exceptPref = areaList = endemicArea[1].replace('を除く都道府県', '').split('、');
+        areaList = PREFECTURE_LIST.filter(p => !exceptPref.includes(p));
+
+    } else {
+        areaList = endemicArea[1].split('、');
+    }
+
     const timestamp = (new Date()).toLocaleString();
     await fs.writeFile('./data/endemic-area.json', JSON.stringify({
         area: areaList,
